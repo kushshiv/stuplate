f924c54cba9b469dad4d5f46793ee736f9ee3e7b
Shivendra Pratap Kushwaha <shivendra@Shivendras-MacBook-Air.local>
2019-04-14 01:10:17 +0530

    Start commit

---
+++ app/routes.py
@@ -0,0 +1,37 @@
+from app import app
+from flask import Flask, render_template, abort
+ 
+PRODUCTS = {
+    'iphone': {
+        'name': 'Coaching Classes',
+        'category': 'Phones',
+        'price':'Starting at 1000',
+    },
+    'galaxy': {
+        'name': 'Hostels',
+        'category': 'Phones',
+        'price':'Starting at 1500',
+    },
+    'ipad-air': {
+        'name': 'Individual rooms/flats',
+        'category': 'Tablets',
+        'price':'Starting at 1500',
+    },
+    'ipad-mini': {
+        'name': 'Examinee rooms',
+        'category': 'Tablets',
+        'price':'Starting at 2500'
+    }
+}
+ 
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', products=PRODUCTS)
+ 
+@app.route('/product/<key>')
+def product(key):
+    product = PRODUCTS.get(key)
+    if not product:
+        abort(404)
+    return render_template('product.html', product=product)
