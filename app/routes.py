02e04694d22a2049d13cc25304c7037c7d89e098
shivendra <shivendra.ds48@gmail.com>
2019-08-05 16:25:02 +0530

    Contact Us page created and base page modified.

--- app/routes.py
+++ app/routes.py
@@ -196,3 +196,7 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/contactUs')
+def contactUs():
+    return render_template('contactUs.html')
